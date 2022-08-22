6a3c8ecadfd396d4afa211f41453a6bf9bb89e51
Emin.lin <emin.lin@zkteco.com>
2022-08-22 11:49:25 +0800

    修改样式

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,6 +2,8 @@ import { extend } from "flarum/extend";
 import app from "flarum/app";
 import LogInButtons from "flarum/components/LogInButtons";
 import QQLogInButton from "./components/QQLogInButton";
+import SettingsPage from 'flarum/components/SettingsPage';
+import Button from 'flarum/components/Button';
 
 app.initializers.add("hamzone-auth-qq", () => {
 
